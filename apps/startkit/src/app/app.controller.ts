b89495a5b4d6747c320656e5708ef63cf862b3f4
LinBo Len <linbolen@gradii.com>
2024-07-06 12:48:09 +0800

    modify ci workflow

--- apps/startkit/src/app/app.controller.ts
+++ apps/startkit/src/app/app.controller.ts
@@ -1,33 +1,33 @@
-import {faker} from '@faker-js/faker';
-import {schema} from '@gradii/fedaco';
-import {Controller, Get} from '@nestjs/common';
-import {UserModel} from './models/user.model';
+import { faker } from '@faker-js/faker';
+import { db, schema } from '@gradii/fedaco';
+import { Controller, Get } from '@nestjs/common';
+import { UserModel } from './models/user.model';
 
 @Controller()
 export class AppController {
-    constructor() {
-    }
+  constructor() {
+  }
 
-    @Get('/init-table')
-    async initTable() {
-        if (!await schema().hasTable('users')) {
-            await schema().create('users', table => {
-                table.increments('id');
-                table.string('username');
-                table.timestamps();
-            });
-        }
+  @Get('/init-table')
+  async initTable() {
+    if (!await schema().hasTable('users')) {
+      await schema().create('users', table => {
+        table.increments('id');
+        table.string('username');
+        table.timestamps();
+      });
     }
+  }
 
-    @Get('/add-user')
-    async addUser() {
-        await UserModel.createQuery().create({
-            username: faker.finance.accountName()
-        });
-    }
+  @Get('/add-user')
+  async addUser() {
+    await UserModel.createQuery().create({
+      username: faker.finance.accountName()
+    });
+  }
 
-    @Get('/list-user')
-    async listUsers() {
-        return await UserModel.createQuery().get();
-    }
+  @Get('/list-user')
+  async listUsers() {
+    return await UserModel.createQuery().get();
+  }
 }
